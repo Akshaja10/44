3b9abcaa9f24fb94a86ef69b2f26cb710ec3cd7d
Akshaja10 <76399647+Akshaja10@users.noreply.github.com>
2023-09-23 10:23:18 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function preload(){
+  watermelon = loadImage("watermelon.png");
+  watermelonCut = loadImage("watermeloncut.png");
+  apple = loadImage("apple.jpg");
+  appleCut = loadImage("applecut.png");
+  orange = loadImage("orange.jpg");
+  orangeCut = loadImage("orangecut.jpg")
+  banana = loadImage("banana.png");
+  bananaCut = loadImage("bananacut.jpg")
+
+  knife = loadImage("knife.png");
+  gameOver = loadImage("gameover.png");
+}
+
+function setup(){
+  canvas = createCanvas(600,600)
+
+}
+
+function draw(){
+  background("blue")
+}
